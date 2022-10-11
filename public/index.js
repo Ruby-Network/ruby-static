0f2609066232f1892a6890424d60607fd7de708f
XxHiroTTVxX <101534563+XxHiroTTVxX@users.noreply.github.com>
2022-10-10 17:34:07 -0700

    Update index.js

--- public/index.js
+++ public/index.js
@@ -18,10 +18,5 @@ form.addEventListener("submit", async (event) => {
 });
 
 function isUrl(val = "") {
-  if (
-    /^http(s?):\/\//.test(val) ||
-    (val.includes(".") && val.substr(0, 1) !== " ")
-  )
-    return true;
-  return false;
+  return (/^http(s?):\/\//.test(val) || val.includes(".") && val.substr(0, 1) !== " ")
 }
