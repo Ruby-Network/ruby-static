809f4f8145d4c695167cf56116bd8ce32a9c8007
XxHiroTTVxX <phoenixmerlinwolf894@gmail.com>
2022-11-22 00:01:10 -0800

    Update title.js

--- public/title.js
+++ public/title.js
@@ -1,12 +1,12 @@
-var get = a => localStorage.getItem(a)
-var set = (a, b) => localStorage.setItem(a, b)
-document.title = get('title') || 'Weeb Central'
-var link = document.createElement('link');
-link.rel = 'icon';
-document.getElementsByTagName('head')[0].appendChild(link);
+var get = (a) => localStorage.getItem(a);
+var set = (a, b) => localStorage.setItem(a, b);
+document.title = get("title") || "Weeb Central";
+var link = document.createElement("link");
+link.rel = "icon";
+document.getElementsByTagName("head")[0].appendChild(link);
 function faviconChange(value) {
-  set('link', value)
-  link.href = get('link') || '/img/favicon.ico';
-  console.log("working " + link.href)
+  set("link", value);
+  link.href = get("link") || "/img/favicon.ico";
+  console.log("working " + link.href);
 }
-link.href = get('link') || '/img/favicon.ico';
+link.href = get("link") || "/img/favicon.ico";
