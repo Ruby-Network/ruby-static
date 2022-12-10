e85c392293ce4c66733517f574cb11447f835813
XxHiroTTVxX <phoenixmerlinwolf894@gmail.com>
2022-12-10 00:53:50 -0800

    Make the css simple and add search suggestions

--- public/js/index.js
+++ public/js/index.js
@@ -27,3 +27,25 @@ function isUrl(val = "") {
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("/sw.js");
 }
+
+const searchInput = document.getElementById("search-input");
+const searchForm = document.getElementById("search-form");
+
+// Listen for changes to the search input
+searchInput.addEventListener("input", async (event) => {
+  // Get the search query from the input field
+  const query = event.target.value;
+
+  // Make a request to the Brave search suggestion API
+  const response = await fetch(
+    `https://suggestions.brave.com/suggestions?q=${encodeURIComponent(query)}`
+  );
+
+  // Parse the response as JSON
+  const suggestions = await response.json();
+
+  // Update the search form with the suggestions
+  searchForm.innerHTML = suggestions
+    .map((suggestion) => `<option value="${suggestion}">`)
+    .join("");
+});
--- public/js/settings.js
+++ public/js/settings.js
@@ -1,29 +1,28 @@
-//Browser Change
-
 function browserChange() {
   var inputVal = document.getElementById("select-browser").value;
   console.log(inputVal);
-  if (inputVal == "google") {
-    localStorage.setItem("urlstart", "https://google.com/search?q=");
-    console.log(localStorage.getItem("urlstart"));
-  }
-  if (inputVal == "duckduckGo") {
-    localStorage.setItem("urlstart", "https://duckduckgo.com/?q=");
-    console.log(localStorage.getItem("urlstart"));
-  }
-  if (inputVal == "brave") {
-    localStorage.setItem("urlstart", "https://search.brave.com/search?q=");
-    console.log(localStorage.getItem("urlstart"));
-  }
-  if (inputVal == "qwant") {
-    localStorage.setItem("urlstart", "https://www.qwant.com/?q=");
-    console.log(localStorage.getItem("urlstart"));
-  }
-  if (inputVal == "yahoo") {
-    localStorage.setItem("urlstart", "https://search.yahoo.com/search?q=");
-    console.log(localStorage.getItem("urlstart"));
-  }
-}
 
-//Proxy Change
-//Not Ready Yet
+  // Use a switch statement to handle the different browser options
+  switch (inputVal) {
+    case "google":
+      localStorage.setItem("urlstart", "https://google.com/search?q=");
+      break;
+    case "duckduckGo":
+      localStorage.setItem("urlstart", "https://duckduckgo.com/?q=");
+      break;
+    case "brave":
+      localStorage.setItem("urlstart", "https://search.brave.com/search?q=");
+      break;
+    case "qwant":
+      localStorage.setItem("urlstart", "https://www.qwant.com/?q=");
+      break;
+    case "yahoo":
+      localStorage.setItem("urlstart", "https://search.yahoo.com/search?q=");
+      break;
+    default:
+      // Set the urlstart to an empty string if the inputVal is not recognized
+      localStorage.setItem("urlstart", "");
+  }
+
+  console.log(localStorage.getItem("urlstart"));
+}
