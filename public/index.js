4d75e01a1b436eb4d94ee2ec15bf1131cab084b4
XxHiroTTVxX <phoenixmerlinwolf894@gmail.com>
2022-12-10 01:09:42 -0800

    add search suggestion

--- public/index.js
+++ public/index.js
@@ -1,4 +1,3 @@
-
 "use strict";
 /**
  * @type {HTMLFormElement}
@@ -34,4 +33,28 @@ form.addEventListener("submit", async (event) => {
 
   const url = search(address.value, searchEngine.value);
   location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+});
+
+/*search Suggestion*/
+
+const searchInput = document.getElementById("uv-form");
+const searchForm = document.getElementById("uv-address");
+
+// Listen for changes to the search input
+searchInput.addEventListener("input", async (event) => {
+  // Get the search query from the input field
+  const query = event.target.value;
+
+  // Make a request to the Brave search suggestion API
+  const response = await fetch(
+    `https://suggestions.brave.com/suggestions?q=${encodeURIComponent(query)}`
+  );
+
+  // Parse the response as JSON
+  const suggestions = await response.json();
+
+  // Update the search form with the suggestions
+  searchForm.innerHTML = suggestions
+    .map((suggestion) => `<option value="${suggestion}">`)
+    .join("");
 });
